17a165c4fffe3e0596bf7f803f39e4c6cb436609
Phung Pha <phungmac@Phungs-MacBook-Pro.local>
2022-12-25 21:22:05 -0600

    edited phone num

--- src/components/common/callBtn.js
+++ src/components/common/callBtn.js
@@ -1,13 +1,14 @@
 import phoneIcon from '../../images/telephone.png';
 import { FiPhoneCall } from 'react-icons/fi';
 import { FaRegCalendarPlus } from 'react-icons/fa';
+import db from '../../constant/db';
 
 function CallButton() {
   return (
     <div className="fixed bottom-1 left-1  w-full mx-auto md:invisible">
       <button className="btn-primary p-4  rounded-lg inline-block">
         <a
-          href="tel:832-779-3378"
+          href={`tel:${db.phone}`}
           className="items-center itext-center  text-xl"
         >
           <FiPhoneCall />

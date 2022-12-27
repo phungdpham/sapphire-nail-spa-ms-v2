d3b6695658ff16de88f993fe94d506b96665a215
Phung Pha <phungmac@Phungs-MacBook-Pro.local>
2022-12-26 23:52:17 -0600

    edited

--- src/components/common/mobileBtn.js
+++ src/components/common/mobileBtn.js
@@ -8,20 +8,20 @@ function MobileButtons() {
     <div className="fixed bottom-0 left-0  w-full mx-auto md:invisible">
       <div className="flex mx-auto btn-primary divide-x-[1px] divide-primary">
         
-        <button className="w-6/12 flex items-center  mx-2 ">
+        <button className="w-6/12 flex items-center  m-2 ">
         
           <a
             href={db.booking}
-            className="items-center mx-auto flex gap-2 text-center text-xl"
+            className="items-center mx-auto flex gap-2 text-center text-lg"
             target="_blank"
           >
             <FaRegCalendarPlus />Book Online
           </a>
         </button>
-        <button className="w-6/12 flex items-center  mx-2  ">
+        <button className="w-6/12 flex items-center  m-2  ">
           <a
             href={`tel:${db.phone}`}
-            className="items-center  mx-auto flex gap-2 text-xl"
+            className="items-center  mx-auto flex gap-2 text-lg"
           >
             <FiPhoneCall /> Call Us
           </a>
